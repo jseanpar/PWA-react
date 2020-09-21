3ccedb6022fb0f698fcb638f6a817644f9c122e9
jose rubilar <jose.rubilar@pad.cl>
2020-09-21 15:36:23 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+![Captura de Recetas](.readme-static/Captura.JPG)
+
+# Curso de PWA con React en Platzi
+
+Este repositorio contiene todo el contenido del curso a medida que construimos una app para ver recetas con la API de MealDB.
+
+Hay un branch por clase para que puedas navegar con facilidad el curso.
+
+## Scripts
+
+- `npm install` para instalar las dependencias
+- `npm run dev` para entorno de desarrollo
+- `npm run build && npm start` para producción
+
+## Licencia
+
+MIT
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Router, Route, Link } from "react-router-dom"
+import Home from './pages/Home'
+import Recipe from './pages/Recipe'
+import Timer from './pages/Timer'
+import './App.css'
+
+import { createBrowserHistory } from 'history'
+import ReactGA from 'react-ga'
+import IfOffline from './components/ifOffline'
+
+const history = createBrowserHistory()
+
+ReactGA.initialize('UA-00000-01')
+
+ReactGA.pageview(window.location.pathname +
+  window.location.search)
+
+history.listen(function (location) {
+  ReactGA.pageview(window.location.pathname +
+    window.location.search)
+})
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router history={history}>
+        <div>
+          <header>
+            <Link to="/">Recetas <IfOffline>Offline</IfOffline></Link>
+            <Link to="/timer"
+              className="timerLink">Timer</Link>
+          </header>
+          <main>
+            <Route exact path="/" component={Home} />
+            <Route path="/recipe/:recipeId" component={Recipe} />
+            <Route path="/timer" component={Timer} />
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
